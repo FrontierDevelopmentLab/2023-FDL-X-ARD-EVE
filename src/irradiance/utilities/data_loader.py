ded47b5b11f13358d5fcc045f00f65e91a1cae16
Will Fawcett <willfaw@gmail.com>
2023-07-25 20:57:25 +0000

    bugfix

--- src/irradiance/utilities/data_loader.py
+++ src/irradiance/utilities/data_loader.py
@@ -299,7 +299,7 @@ class ZarrIrradianceDataModule(pl.LightningDataModule):
                 normalizations_aia[wavelength]["image_count"] += wavelength_data.shape[0] 
                 normalizations_aia[wavelength]["max"] = max(wavelength_data.max().compute(), normalizations_aia[wavelength]["max"])
 
-            width, height = wavelength_data.shape
+            batch, width, height = wavelength_data.shape
             normalizations_aia[wavelength]["count"] = normalizations_aia[wavelength]["image_count"] * width * height # number of pixels
             normalizations_aia[wavelength]["mean"] = normalizations_aia[wavelength]["sum"] / normalizations_aia[wavelength]["count"]
             return normalizations_aia
