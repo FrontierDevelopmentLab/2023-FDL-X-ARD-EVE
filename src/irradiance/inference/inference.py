387f8e291cc48ba4569cee241227583d7b81eb53
superl44tcoder <richardagalvez@gmail.com>
2023-08-11 07:59:32 +0000

    Inference comparison and generation WIP

--- src/irradiance/inference/inference.py
+++ src/irradiance/inference/inference.py
@@ -5,6 +5,9 @@ import pandas as pd
 from tqdm import tqdm
 import zarr
 from datetime import datetime
+import pandas as pd
+
+import plotly.express as px
 
 # from fastapi import FastAPI
 # app = FastAPI()
@@ -23,7 +26,7 @@ class IrradianceInferenceModel:
         self.eve_ions.sort()
         self.aia_normalizations = state["normalizations"]["AIA"]
 
-        self.aia_data = zarr.group(zarr.DirectoryStore("/mnt/sdomlv2_small/sdomlv2_small.zarr"))
+        self.aia_data = zarr.group(zarr.DirectoryStore("/mnt/sdomlv2_full/sdomlv2.zarr"))
 
 
     def predict(self, aia_image, forward_passes=0):
@@ -77,6 +80,46 @@ def run_one_off_inferences():
                 continue
         pred_irradiance.to_parquet(f"/home/jupyter/output_inferences/run1/pred_irradiance_{year}.parquet")
         print(f"Saved year: {year}, shape: {pred_irradiance.shape}")
+
+
+def get_eve_data(year=2011):
+    eve_zarr = zarr.group(zarr.DirectoryStore("/mnt/sdomlv2_full/sdomlv2_eve.zarr"))["MEGS-A"]
+    eve_timestamps = eve_zarr["Time"][:]
+    eve_timestamps = pd.to_datetime(eve_timestamps)
+    eve_ions = inference_model.eve_ions
+    eve_data = pd.DataFrame(index=eve_timestamps, columns=eve_ions)
+    for ion in eve_ions: eve_data[ion] = eve_zarr[ion][:]
+
+    eve_data = eve_data[eve_data>-1]
+    eve_data.dropna(inplace=True)
+    eve_data.sort_index(inplace=True)
+
+    eve_data = eve_data.loc[f"{year}-01-01":f"{year}-12-31"]
+
+    return eve_data
+
+
+def get_prediction_data(year=2011):
+    predictions = pd.read_parquet(f"/home/jupyter/output_inferences/run1/pred_irradiance_{year}.parquet")
+    predictions.dropna(inplace=True)
+    return predictions
+
+eve_data = get_eve_data()
+predictions = get_prediction_data()
+
+def comparison_plot(date_start, date_end, eve_data, predictions):
+    predictions = predictions.loc[date_start:date_end]
+    eve_data = eve_data.loc[date_start:date_end]
+
+    fig = px.line()
+    for ion in inference_model.eve_ions[:2]:
+        fig.add_scatter(x=predictions.index, y=predictions[ion], name=f"Predicted {ion}")
+        fig.add_scatter(x=eve_data.index, y=eve_data[ion], name=f"Actual {ion}")
+    fig.show()
+
+
+comparison_plot("2011-01-01", "2011-01-02", eve_data, predictions)
+
 
 
 # @app.get("/")
