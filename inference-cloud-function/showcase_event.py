30c4a959b3fba555b3f228b6e8865d093b92f2f9
l33tCodeHackerRichardGGGGG <richardagalvez@gmail.com>
2023-08-16 01:07:30 +0000

    Sent off 2013 and 2014

--- inference-cloud-function/showcase_event.py
+++ inference-cloud-function/showcase_event.py
@@ -4,6 +4,7 @@ import subprocess
 from concurrent.futures import ThreadPoolExecutor
 from google.cloud import pubsub_v1
 
+import time
 
 token = subprocess.run('gcloud auth print-identity-token', shell=True, capture_output=True, text=True).stdout.strip()
 headers = {"Authorization": f"bearer {token}", "Content-Type": "application/json"}
@@ -21,20 +22,25 @@ def get_date_list(begin_date, end_date):
 
 def publish_message(data):
     future = publisher.publish(topic_name, data=data.encode("utf-8"))
+    time.sleep(0.1)
     return future
 
 
-date_list = get_date_list(begin_date='2011-01-01T01:00:00', end_date='2011-02-01 00:00:00')
-futures = []
-for inference_date in date_list:
-    timestamp = inference_date.strftime('%Y-%m-%dT%H:%M:%S')
-    print(f"Submitting: {timestamp} to pubsub")
+for year in range(2013, 2015):
 
-    data = str({"timestamp": timestamp, "api_endpoint_url": url})
-    future = publish_message(data)
-    futures.append(future)
+    date_list = get_date_list(begin_date=f'{year}-01-01 00:00:00', end_date=f'{year}-12-31 23:48:00')
+
+    print(f"Sending {len(date_list)} messages for year {year}")
+
+    futures = []
+    for inference_date in date_list:
+        timestamp = inference_date.strftime('%Y-%m-%dT%H:%M:%S')
+
+        data = str({"timestamp": timestamp, "api_endpoint_url": url})
+        future = publish_message(data)
+        futures.append(future)
 
 
-results = [future.result() for future in futures]
-print(results)
-print(f"Published {len(results)} messages to {topic_name}")
+    results = [future.result() for future in futures]
+    print(f"Published {len(results)} messages for year {year}")
+    time.sleep(1800)
